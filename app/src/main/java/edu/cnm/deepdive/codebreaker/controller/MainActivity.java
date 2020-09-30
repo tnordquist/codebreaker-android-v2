1c031b64fda17a805505bea87269c77cc77ce7ad
Nick Bennett <nick@nickbenn.com>
2020-09-30 14:27:21 -0600

    Started the options menu.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
@@ -1,5 +1,7 @@
 package edu.cnm.deepdive.codebreaker.controller;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
@@ -7,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import edu.cnm.deepdive.codebreaker.R;
@@ -38,6 +41,26 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     adapter =
         new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<Guess>());
     guessList.setAdapter(adapter);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    getMenuInflater().inflate(R.menu.options, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    boolean handled = true;
+    switch (item.getItemId()) {
+      case R.id.new_game:
+        // TODO Start new game.
+        break;
+      default:
+        handled = super.onOptionsItemSelected(item);
+    }
+    return handled;
   }
 
   @Override
