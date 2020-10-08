3191656ab47983b4d8d9f1713f5671bf5df08e00
Nick Bennett <nick@nickbenn.com>
2020-10-08 15:12:19 -0600

    Completed fragment changeover.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/GameFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/GameFragment.java
@@ -1,6 +1,5 @@
 package edu.cnm.deepdive.codebreaker.controller;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.InputFilter;
@@ -11,7 +10,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -117,11 +115,6 @@ public class GameFragment extends Fragment implements InputFilter {
     });
     viewModel.getSolved().observe(lifecycleOwner, solved ->
         binding.guessControls.setVisibility(solved ? View.INVISIBLE : View.VISIBLE));
-    viewModel.getThrowable().observe(lifecycleOwner, (throwable) -> {
-      if (throwable != null) {
-        Toast.makeText(activity, throwable.getLocalizedMessage(), Toast.LENGTH_LONG).show();
-      }
-    });
   }
 
   private void recordGuess() {
--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
@@ -1,43 +1,24 @@
 package edu.cnm.deepdive.codebreaker.controller;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.text.InputFilter;
-import android.text.Spanned;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import androidx.lifecycle.ViewModelProvider;
 import edu.cnm.deepdive.codebreaker.R;
-import edu.cnm.deepdive.codebreaker.adapter.GuessAdapter;
-import edu.cnm.deepdive.codebreaker.databinding.ActivityMainBinding;
 import edu.cnm.deepdive.codebreaker.viewmodel.MainViewModel;
-import java.util.HashMap;
-import java.util.Map;
 
-public class MainActivity extends AppCompatActivity implements InputFilter {
-
-  private static final String INVALID_CHAR_PATTERN = String.format("[^%s]", MainViewModel.POOL);
-  private static final int[] colorValues =
-      {Color.RED, 0xffffa500, Color.YELLOW, Color.GREEN, Color.BLUE, 0xff4b0082, 0xffee82ee};
-  private static final Map<Character, Integer> colorMap =
-      buildColorMap(MainViewModel.POOL.toCharArray(), colorValues);
+public class MainActivity extends AppCompatActivity {
 
   private MainViewModel viewModel;
-  private GuessAdapter adapter;
-  private int codeLength;
-  private ActivityMainBinding binding;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    binding = ActivityMainBinding.inflate(getLayoutInflater());
-    setContentView(binding.getRoot());
-    setupViews();
+    setContentView(R.layout.activity_main);
     setupViewModel();
   }
 
@@ -51,13 +32,8 @@ public class MainActivity extends AppCompatActivity implements InputFilter {
   @Override
   public boolean onOptionsItemSelected(@NonNull MenuItem item) {
     boolean handled = true;
+    //noinspection SwitchStatementWithTooFewBranches
     switch (item.getItemId()) {
-      case R.id.new_game:
-        startGame();
-        break;
-      case R.id.restart_game:
-        restartGame();
-        break;
       case R.id.settings:
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
@@ -68,64 +44,13 @@ public class MainActivity extends AppCompatActivity implements InputFilter {
     return handled;
   }
 
-  @Override
-  public CharSequence filter(CharSequence source, int sourceStart, int sourceEnd,
-      Spanned dest, int destStart, int destEnd) {
-    String modifiedSource = source.toString().toUpperCase().replaceAll(INVALID_CHAR_PATTERN, "");
-    StringBuilder builder = new StringBuilder(dest);
-    builder.replace(destStart, destEnd, modifiedSource);
-    if (builder.length() > codeLength) {
-      modifiedSource =
-          modifiedSource.substring(0, modifiedSource.length() - (builder.length() - codeLength));
-    }
-    int newLength = dest.length() - (destEnd - destStart) + modifiedSource.length();
-    binding.submit.setEnabled(newLength == codeLength);
-    return modifiedSource;
-  }
-
-  private void setupViews() {
-    binding.guess.setFilters(new InputFilter[]{this});
-    binding.submit.setOnClickListener((view) -> recordGuess());
-  }
-
   private void setupViewModel() {
-    adapter = new GuessAdapter(MainActivity.this, colorMap);
     viewModel = new ViewModelProvider(this).get(MainViewModel.class);
-    viewModel.getGame().observe(this, (game) -> {
-      adapter.clear();
-      adapter.addAll(game.getGuesses());
-      binding.guessList.setAdapter(adapter);
-      binding.guessList.setSelection(adapter.getCount() - 1);
-      codeLength = game.getLength();
-      binding.guess.setText("");
-    });
-    viewModel.getSolved().observe(this, solved ->
-        binding.guessControls.setVisibility(solved ? View.INVISIBLE : View.VISIBLE));
     viewModel.getThrowable().observe(this, (throwable) -> {
       if (throwable != null) {
         Toast.makeText(this, throwable.getLocalizedMessage(), Toast.LENGTH_LONG).show();
       }
     });
-  }
-
-  private void recordGuess() {
-    viewModel.guess(binding.guess.getText().toString().trim().toUpperCase());
-  }
-
-  private void startGame() {
-    viewModel.startGame();
-  }
-
-  private void restartGame() {
-    viewModel.restartGame();
-  }
-
-  private static Map<Character, Integer> buildColorMap(char[] chars, int[] values) {
-    Map<Character, Integer> colorMap = new HashMap<>();
-    for (int i = 0; i < chars.length; i++) {
-      colorMap.put(chars[i], values[i]);
-    }
-    return colorMap;
   }
 
 }
