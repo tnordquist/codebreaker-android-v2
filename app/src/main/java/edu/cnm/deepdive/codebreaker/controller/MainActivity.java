ace5c98beb5f7ffb3c3704fa5a631c38f903cf4e
Nick Bennett <nick@nickbenn.com>
2020-10-20 17:51:39 -0600

    Implemented fragment navigation.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
@@ -1,25 +1,34 @@
 package edu.cnm.deepdive.codebreaker.controller;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 import edu.cnm.deepdive.codebreaker.R;
 import edu.cnm.deepdive.codebreaker.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
   private MainViewModel viewModel;
+  private NavOptions navOptions;
+  private NavController navController;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     setupViewModel();
+    setupNavigation();
   }
 
   @Override
@@ -35,13 +44,18 @@ public class MainActivity extends AppCompatActivity {
     //noinspection SwitchStatementWithTooFewBranches
     switch (item.getItemId()) {
       case R.id.settings:
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
+        navController.navigate(R.id.navigation_settings, null, navOptions);
         break;
       default:
         handled = super.onOptionsItemSelected(item);
     }
     return handled;
+  }
+
+  @Override
+  public boolean onSupportNavigateUp() {
+    onBackPressed();
+    return true;
   }
 
   private void setupViewModel() {
@@ -51,6 +65,21 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, throwable.getLocalizedMessage(), Toast.LENGTH_LONG).show();
       }
     });
+  }
+
+  private void setupNavigation() {
+    AppBarConfiguration appBarConfiguration =
+        new AppBarConfiguration.Builder(R.id.navigation_game, R.id.navigation_settings)
+            .build();
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    navOptions = new NavOptions.Builder()
+        .setPopUpTo(R.id.navigation_game, false)
+        .build();
+    navController =
+        ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment))
+            .getNavController();
+    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
   }
 
 }
---
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/SettingsFragment.java
@@ -0,0 +1,28 @@
+package edu.cnm.deepdive.codebreaker.controller;
+
+import android.os.Bundle;
+import android.view.View;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+import edu.cnm.deepdive.codebreaker.R;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+  @Override
+  public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+    setPreferencesFromResource(R.xml.preferences, rootKey);
+  }
+
+  @SuppressWarnings("ConstantConditions")
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+    actionBar.setDisplayHomeAsUpEnabled(true);
+    actionBar.setDisplayShowHomeEnabled(true);
+  }
+
+}
